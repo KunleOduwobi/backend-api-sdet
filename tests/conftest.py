d7d7a8dc54ec4d8ca6823943b3f89a62d8b423a5
KunleOduwobi <kunleodu@gmail.com>
2026-02-26 09:28:01 +0100

    check empty api key

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,4 +9,6 @@ import os
 @pytest.fixture(scope="session")
 def api_client():
     api_key = os.getenv("REQRES_API_KEY")
+    if not api_key:
+        raise ValueError("API key not found in environment variables")
     return APIClient("https://reqres.in", api_key)
